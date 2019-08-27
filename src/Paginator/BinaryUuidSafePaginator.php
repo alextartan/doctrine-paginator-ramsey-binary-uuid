bf11f13e7b111a2bfa2f21c74dacf57d5f363047
Alex <alex.tartan@gmail.com>
2019-08-28 02:16:43 +0300

    cleanup

--- src/Paginator/BinaryUuidSafePaginator.php
+++ src/Paginator/BinaryUuidSafePaginator.php
@@ -21,7 +21,7 @@ use function count;
  * @method void unbindUnusedQueryParams(Query $query)
  *
  * @property Query $query
- * @property bool $fetchJoinCollection
+ * @property bool  $fetchJoinCollection
  */
 class BinaryUuidSafePaginator extends Paginator
 {
@@ -31,9 +31,6 @@ class BinaryUuidSafePaginator extends Paginator
     }
 
     /**
-     * @param string $name
-     * @param array  $arguments
-     *
      * @return mixed
      */
     public function __call(string $name, array $arguments)
@@ -42,8 +39,6 @@ class BinaryUuidSafePaginator extends Paginator
     }
 
     /**
-     * @param string $name
-     *
      * @return mixed
      */
     public function __get(string $name)
@@ -53,49 +48,44 @@ class BinaryUuidSafePaginator extends Paginator
 
     public function getIterator(): ArrayIterator
     {
+        if (!$this->fetchJoinCollection) {
+            return parent::getIterator();
+        }
+
         $offset = $this->query->getFirstResult();
         $length = $this->query->getMaxResults();
 
-        if ($this->fetchJoinCollection) {
-            $subQuery = $this->cloneQuery($this->query);
+        $subQuery = $this->cloneQuery($this->query);
 
-            if ($this->useOutputWalker($subQuery)) {
-                $subQuery->setHint(Query::HINT_CUSTOM_OUTPUT_WALKER, LimitSubqueryOutputWalker::class);
-            } else {
-                $this->appendTreeWalker($subQuery, LimitSubqueryWalker::class);
-                $this->unbindUnusedQueryParams($subQuery);
-            }
-
-            $subQuery->setFirstResult($offset)->setMaxResults($length);
-
-            $ids = array_map('current', $subQuery->getScalarResult());
-            foreach ($ids as $key => $id) {
-                if ($id instanceof UuidInterface) {
-                    $ids[$key] = $id->getBytes();
-                }
-            }
-
-            $whereInQuery = $this->cloneQuery($this->query);
-            // don't do this for an empty id array
-            if (count($ids) === 0) {
-                return new ArrayIterator([]);
-            }
-
-            $this->appendTreeWalker($whereInQuery, WhereInWalker::class);
-            $whereInQuery->setHint(WhereInWalker::HINT_PAGINATOR_ID_COUNT, count($ids));
-            $whereInQuery->setFirstResult(null)->setMaxResults(null);
-            $whereInQuery->setParameter(WhereInWalker::PAGINATOR_ID_ALIAS, $ids);
-            $whereInQuery->setCacheable($this->query->isCacheable());
-
-            $result = $whereInQuery->getResult($this->query->getHydrationMode());
+        if ($this->useOutputWalker($subQuery)) {
+            $subQuery->setHint(Query::HINT_CUSTOM_OUTPUT_WALKER, LimitSubqueryOutputWalker::class);
         } else {
-            $result = $this->cloneQuery($this->query)
-                           ->setMaxResults($length)
-                           ->setFirstResult($offset)
-                           ->setCacheable($this->query->isCacheable())
-                           ->getResult($this->query->getHydrationMode());
+            $this->appendTreeWalker($subQuery, LimitSubqueryWalker::class);
+            $this->unbindUnusedQueryParams($subQuery);
         }
 
-        return new ArrayIterator($result);
+        $subQuery->setFirstResult($offset)->setMaxResults($length);
+
+        $ids = array_map('current', $subQuery->getScalarResult());
+        // don't do this for an empty id array
+        if (count($ids) === 0) {
+            return new ArrayIterator([]);
+        }
+
+        foreach ($ids as $key => $id) {
+            if ($id instanceof UuidInterface) {
+                $ids[$key] = $id->getBytes();
+            }
+        }
+
+        $whereInQuery = $this->cloneQuery($this->query);
+
+        $this->appendTreeWalker($whereInQuery, WhereInWalker::class);
+        $whereInQuery->setHint(WhereInWalker::HINT_PAGINATOR_ID_COUNT, count($ids));
+        $whereInQuery->setFirstResult(null)->setMaxResults(null);
+        $whereInQuery->setParameter(WhereInWalker::PAGINATOR_ID_ALIAS, $ids);
+        $whereInQuery->setCacheable($this->query->isCacheable());
+
+        return new ArrayIterator($whereInQuery->getResult($this->query->getHydrationMode()));
     }
 }
